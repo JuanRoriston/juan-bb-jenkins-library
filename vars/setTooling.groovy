bd0cfb2c418485e378adca314871ea3f2db22442
JuanRoriston <juanroris@gmail.com>
2021-08-24 14:42:32 +0200

    fix: removed echo

--- vars/setTooling.groovy
+++ vars/setTooling.groovy
@@ -1,5 +1,4 @@
 def call() {
     // Install the Maven version configured as "juan-bb-Mavin-installation" and add it to the path.
-    sh "echo Setting maven"
     maven "juan-bb-Mavin-installation"
 }
