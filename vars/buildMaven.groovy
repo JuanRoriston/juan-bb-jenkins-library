10c58262624e11abaef7fd07c9b41fddbe7d1938
JuanRoriston <juanroris@gmail.com>
2021-08-25 20:55:40 +0200

    Changed SSH to HTTPS

--- vars/buildMaven.groovy
+++ vars/buildMaven.groovy
@@ -2,7 +2,12 @@ def call() {
     // Get some code from a GitHub repository
     git branch: 'main',
     credentialsId: '70a1ffd0-ecd5-41fa-801a-5e52b996da75',
-    url: 'git@github.com:JuanRoriston/juan-bb-hello-world.git' 
+
+    // With SSH access
+    // url: 'git@github.com:JuanRoriston/juan-bb-hello-world.git'
+
+    // Without HTTPS access
+    url: 'https://github.com/JuanRoriston/juan-bb-jenkins-library.git'
 
     // Test Maven on a Unix agent.
     sh "mvn test -f ./my-app/"
